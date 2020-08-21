b50c2e17e77e14b3f42a85d982bc2bd07fe2500a
DevHyperCoder <arduinoleo88@gmail.com>
2020-08-21 08:06:13 +0530

    Moved to dir stucture

--- virtualresources/webapp/home.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Home page
-def home(request):
-    return render(request,"home.html")
-
--- virtualresources/webapp/reach_views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render,redirect
-
-# About page
-def about_page(request):
-    return render(request,"about.html")
